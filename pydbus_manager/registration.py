1cd817e213f5ac81452a8b2dc242e331aaa5409c
Seebz <sebastien@seebz.net>
2018-05-18 00:18:53 +0200

    Implementation of `InterfacesAdded` (ugly but working)

--- pydbus_manager/registration.py
+++ pydbus_manager/registration.py
@@ -1,26 +1,44 @@
+from gi.repository import Gio
+from pydbus import Variant
 
 class RegistrationMixin:
 	__slots__ = ()
 
 	def register_object(self, path, object, node_info):
 		"""Register object on the bus."""
-		bus = self.bus
-		bus_name = self.bus_name
-
-		registration = bus.register_object(path, object, node_info)
+		manager = self
 
 		def at_init():
-			print("at_init()")
-			proxy = self.get(path)
-			# TODO
-		at_init()
+			proxy = manager.get(path)
+
+			xml = proxy["org.freedesktop.DBus.Introspectable"].Introspect()
+			node_info = Gio.DBusNodeInfo.new_for_xml(xml)
+			interfaces = node_info.interfaces
+
+			interfaces_and_properties = {}
+			for iface in interfaces:
+				interfaces_and_properties[iface.name] = {}
+
+				for prop in iface.properties:
+					try:
+						value = getattr(object, prop.name)
+						variant = Variant(prop.signature, value)
+
+						interfaces_and_properties[iface.name][prop.name] = variant
+					except:
+						pass
+
+			manager.InterfacesAdded(path, interfaces_and_properties)
 
 		def at_exit():
-			print("at_exit()")
-			proxy = self.get(path)
+			proxy = manager.get(path)
 			interfaces = [base.__name__ for base in type(proxy).__bases__]
-			self.InterfacesRemoved(path, interfaces)
 
+			manager.InterfacesRemoved(path, interfaces)
+
+		registration = manager.bus.register_object(path, object, node_info)
 		registration._at_exit(at_exit)
+
+		at_init()
 
 		return registration
